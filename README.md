f9403840ee8ba9b2f2e7fdc2027a751bfd0c17ac
jshackles <jshackles@gmail.com>
2014-07-30 17:23:57 -0700

    BUILD: Version 0.2

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-idle_master
+Idle Master
 ===========
+
+This program will determine which of your Steam games still have Steam Trading Card drops remaining, and will go through each application to simulate you being “in-game” so that cards will drop.  It will check periodically to see if the game you’re idling has card drops remaining.  When only one drop remains, it will start checking more frequently.  When the game you’re idling has no more cards, it’ll move on to the next game.  When no more cards are available, the program will terminate.
+
+Requirements
+-------
+
+This application requires Steam to be open and for you to be logged in.  This program is designed to work with Microsoft Windows.
+
+Setup
+-------
+
+Please read the included Setup Instructions.pdf file for detailed setup instructions and frequently asked questions.
+
+Authors
+-------
+
+jshackles and Stumpokapow
+
+steam-idle was writen in C# using Steamworks.NET and CSteamworks by Riley Labrecque
+
+License
+-------
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation.  A copy of the GNU General Public License can be found at http://www.gnu.org/licenses/.  For your convenience, a copy of this license is included.
---
+++ Source/Idle Master/start.py
@@ -0,0 +1,164 @@
+import requests
+import cookielib
+import bs4
+import time
+import subprocess
+import sys
+import os
+import json
+
+try:
+	authData={}
+	execfile("./settings.txt",authData)
+except:
+	print "Error loading config file"
+	sys.exit()
+	
+if not authData["myProfileURL"]:
+	print "No Steam profile entered"
+	sys.exit()
+	
+if not authData["sessionid"]:
+	print "No sessionid set"
+	sys.exit()
+	
+if not authData["steamLogin"]:
+	print "No steamLogin set"
+	sys.exit()
+
+def generateCookies():
+	global authData
+	try:
+		cookies = dict(sessionid=authData["sessionid"], steamLogin=authData["steamLogin"])
+	except:
+		print "Error setting cookies"
+		sys.exit()
+
+	return cookies
+
+def dropDelay(numDrops):
+	if numDrops>1:
+		baseDelay = (15*60)
+	else:
+		baseDelay = (5*60)
+	return baseDelay
+	
+def idleOpen(appID):
+    try:
+		print "Starting game " + getAppName(appID) + " to idle cards"
+		subprocess.Popen("steam-idle.exe "+str(appID))
+    except:
+        print "Error launching steam-idle with game ID "+str(appID)
+        sys.exit()
+
+def idleClose(appID):
+    try:
+        print "Closing game " + getAppName(appID) 
+        os.system("taskkill.exe -im steam-idle.exe /F")
+    except:
+        print "Error closing game. Exiting."
+        sys.exit()
+
+def chillOut(appID):
+    print "Suspending operation for "+getAppName(appID)
+    idleClose(appID)
+    stillDown = True
+    while stillDown:
+        print "Sleeping for 5 minutes."
+        time.sleep(5*60)
+        try:
+            rBadge = requests.get(authData["myProfileURL"]+"/gamecards/"+str(k)+"/",cookies=cookies)
+            indBadgeData = bs4.BeautifulSoup(rBadge.text)
+            badgeLeftString = indBadgeData.find_all("span",{"class": "progress_info_bold"})[0].contents[0]
+            if "card drops" in badgeLeftString:
+                stillDown = False
+        except:
+            print "Still unable to find drop info."
+    # Resume operations.
+    idleOpen(appID)
+	
+def getAppName(appID):
+	try:
+		api = requests.get("http://store.steampowered.com/api/appdetails/?appids="+str(appID)+"&filters=basic")
+		api_data = json.loads(api.text)
+		return api_data[str(appID)]["data"]["name"]
+	except:
+		return "app "+str(appID)
+
+try:
+	cookies = generateCookies()
+	r = requests.get(authData["myProfileURL"]+"/badges/",cookies=cookies)
+except:
+	print "Error reading badge page"
+	sys.exit()
+	
+print "Finding games that have card drops remaining"
+
+try:
+	badgesLeft = {}
+	badgePageData = bs4.BeautifulSoup(r.text)
+	badgeSet = badgePageData.find_all("div",{"class": "badge_title_stats"})
+except:
+	print "Error finding drop info"
+	sys.exit()
+
+for badge in badgeSet:
+	try:
+		dropCount = badge.find_all("span",{"class": "progress_info_bold"})[0].contents[0]
+		if "No card drops" in dropCount:
+			continue
+		else:
+			# Remaining drops
+			dropCountInt, junk = dropCount.split(" ",1)
+			dropCountInt = int(dropCountInt)
+			linkGuess = badge.find_parent().find_parent().find_parent().find_all("a")[0]["href"]
+			junk, badgeId = linkGuess.split("/gamecards/",1)
+			badgeId = int(badgeId.replace("/",""))
+			badgesLeft[badgeId] = dropCountInt
+	except:
+		continue
+		
+print "Idle Master needs to idle " + str(len(badgesLeft)) + " games"
+
+for k, v in badgesLeft.items():
+	delay = dropDelay(int(v))
+	stillHaveDrops=1
+	numCycles=50
+	maxFail=2
+	
+	idleOpen(k)
+
+	while stillHaveDrops==1:
+		try:
+			print "Sleeping for "+str(delay / 60)+" minutes"
+			time.sleep(delay)
+			numCycles-=1
+			if numCycles<1: # Sanity check against infinite loop
+				stillHaveDrops=0
+
+			print "Checking to see if "+getAppName(k)+" has remaining card drops"
+			rBadge = requests.get(authData["myProfileURL"]+"/gamecards/"+str(k)+"/",cookies=cookies)
+			indBadgeData = bs4.BeautifulSoup(rBadge.text)
+			badgeLeftString = indBadgeData.find_all("span",{"class": "progress_info_bold"})[0].contents[0]
+			if "No card drops" in badgeLeftString:
+				print "No card drops remaining"
+				stillHaveDrops=0
+			else:
+				dropCountInt, junk = badgeLeftString.split(" ",1)
+				dropCountInt = int(dropCountInt)
+				delay = dropDelay(dropCountInt)
+				print getAppName(k) + " has "+str(dropCountInt)+" card drops remaining"
+		except:
+			if maxFail>0:
+				print "Error checking if drops are done, number of tries remaining: "+str(maxFail)
+				maxFail-=1
+			else:
+				# Suspend operations until Steam can be reached.
+				chillOut(k)
+				maxFail+=1
+			break
+
+	idleClose(k)
+	print "Successfully completed idling cards for "+getAppName(k)
+
+print "Successfully completed idling process"
