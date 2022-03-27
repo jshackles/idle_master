07af8ba187cda8db62ec2e9fbdf44fc53a27745e
Jonas Nilson <JonasNilson@users.noreply.github.com>
2022-03-27 13:28:54 +0200

    Version bump to 1.8
    + removes debugging file for release build

--- Source/IdleMasterExtended/Properties/AssemblyInfo.cs
+++ Source/IdleMasterExtended/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -31,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7")]
-[assembly: AssemblyFileVersion("1.7")]
+[assembly: AssemblyVersion("1.8")]
+[assembly: AssemblyFileVersion("1.8")]
+[assembly: NeutralResourcesLanguage("en")]
